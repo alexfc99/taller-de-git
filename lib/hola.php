1ed56c4f266c0e02a15a07b13087cdd43b243bc8
Alex <alufat2056@ieselcaminas.org>
2024-09-23 10:01:29 +0000

    Rebase de hola usa la clase HolaMundo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,8 @@
 <?php
 // Autor: Alex <alexfatuc99@gmail.com>
-print "Introduce tu nombre:"
+require('HolaMundo.php');
+
+print "Introduce tu nombre:";
 $nombre = trim(fgets(STDIN));
-@print "Hola, {$nombre}\n";
+print new HolaMundo($nombre);
 ?>
